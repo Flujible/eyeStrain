41b699463d222bdf36cac422c9076451b83f957f
George Palmer-Bryant <georgepalmerbryant@gmail.com>
2023-08-18 19:32:22 +0100

    refactor: remove unneeded files

--- netflify/functions/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => res.send("Hello world!"));
-
-app.listen(port, () => console.log(`App listening on port ${port}`));
